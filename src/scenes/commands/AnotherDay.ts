26d1a1094a642f53e8e8b1f152836a23745e4471
Electroplayer <vitaly.hawanski@yandex.ru>
2022-10-25 23:44:14 +0300

    Добавлена новая группа команд
    2 команды, которые работают в связке и позволяют смотреть расписание на конкретный день.

---
+++ src/scenes/commands/AnotherDay.ts
@@ -0,0 +1,30 @@
+import { Message } from "node-telegram-bot-api";
+import Command from "../../structures/Command.js";
+import User from "../../structures/User.js";
+import Cache from "../../lib/Cache.js";
+import { anotherDay } from "../../lib/Keyboards.js";
+
+export default class AnotherDayCommand extends Command {
+    name = ["Выбрать другой день"];
+    sceneName = ["main"];
+
+    async exec(user: User, msg: Message): Promise<void> {
+        if (!user.group) {
+            Cache.bot.sendMessage(
+                user.id,
+                "У меня нет данных о тебе. Напиши /start"
+            );
+            return;
+        }
+
+        if (msg.chat.type == "group") return;
+
+        Cache.bot.sendMessage(user.id, "Выбери дату", {
+            reply_markup: {
+                keyboard: anotherDay,
+                resize_keyboard: true,
+                one_time_keyboard: true,
+            },
+        });
+    }
+}
